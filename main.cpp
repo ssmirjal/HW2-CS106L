f70f4c0870d91a5f5d4ee145d18bcbd9ab6872bf
ssmirjal <ssmirjal@stanford.edu>
2015-01-03 12:13:07 -0800

    This is old stuff

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW2 CS106L
+//
+//  Created by Shahab Mirjalili on 5/4/13.
+//  Copyright (c) 2013 Shahab Mirjalili. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World"<<endl;
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+//  main.h
+//  HW2 CS106L
+//
+//  Created by Shahab Mirjalili on 5/4/13.
+//  Copyright (c) 2013 Shahab Mirjalili. All rights reserved.
+//
+
+#ifndef __HW2_CS106L__main__
+#define __HW2_CS106L__main__
+
+#include <iostream>
+
+#endif /* defined(__HW2_CS106L__main__) */
